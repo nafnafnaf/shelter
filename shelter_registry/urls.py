d73a3cdcac07b1cbcb80b5c1c873b1cdb3f4a835
Dimi <info@nafnaf.gr>
2025-12-14 20:40:46 +0200

    added endpoint

--- shelter_registry/urls.py
+++ shelter_registry/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
+    path('', include('animals.urls')),
 ]
 
 # Serve media files in development
