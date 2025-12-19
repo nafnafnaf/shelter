a7b523ea0bfbd2dce6cec356f7939d974adeb242
Dimi <info@nafnaf.gr>
2025-12-19 19:52:39 +0200

    Update URLs configuration

--- shelter_registry/urls.py
+++ shelter_registry/urls.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 from rest_framework.routers import DefaultRouter
 from animals.api.views import AnimalViewSet, MedicalRecordViewSet, AnimalPhotoViewSet
-from django.views.generic import TemplateView
 
 # API Router
 router = DefaultRouter()
@@ -16,10 +16,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
-    path('', include('animals.urls')),  # Include animals app URLs (adopt/, qr/, etc.)
+    path('', include('animals.urls')),  # Animals URLs at root (includes /adopt/, /qr/, etc)
 ]
-
-# Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
