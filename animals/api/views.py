e129ac94453e27c4fb7012cab2c0cc67605bd83e
Nafpigos Dimi <dimitrios.nafpigos@bossinfo.ch>
2025-12-22 12:58:24 +0200

    Simplify permissions: remove ShelterUser, use Django staff/superuser

--- animals/api/views.py
+++ animals/api/views.py
@@ -6,57 +6,61 @@ from django.db.models import Q
 from ..models import Animal, MedicalRecord, AnimalPhoto
 from .serializers import AnimalSerializer, MedicalRecordSerializer, AnimalPhotoSerializer
 
-class IsAdminOrEmployeeReadOnly(permissions.BasePermission):
+class IsStaffOrReadOnly(permissions.BasePermission):
+    """
+    Simplified permission: Staff users have full access, others read-only
+    """
     def has_permission(self, request, view):
         if not request.user.is_authenticated:
             return False
         
-        try:
-            shelter_user = ShelterUser.objects.get(user=request.user)
-            if shelter_user.role == 'admin':
-                return True
-            elif shelter_user.role == 'employee':
-                return request.method in ['GET', 'POST', 'PUT', 'PATCH', 'HEAD', 'OPTIONS']
-            return False
-        except ShelterUser.DoesNotExist:
-            return False
+        # Staff/superusers can do anything
+        if request.user.is_staff or request.user.is_superuser:
+            return True
+        
+        # Regular authenticated users have read-only access
+        return request.method in permissions.SAFE_METHODS
 
 class AnimalViewSet(viewsets.ModelViewSet):
     serializer_class = AnimalSerializer
-    permission_classes = [IsAdminOrEmployeeReadOnly]
+    permission_classes = [IsStaffOrReadOnly]
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['species', 'gender', 'behavior', 'adoption_status', 'public_visibility']
-    search_fields = ['name', 'chip_id', 'capture_location']
+    filterset_fields = ['species', 'gender', 'behavior', 'adoption_status', 'public_visibility', 'shelter']
+    search_fields = ['name', 'chip_id', 'capture_location', 'shelter']
     ordering_fields = ['created_at', 'name', 'entry_date']
-    ordering = ['-created_at']
-
+    
     def get_queryset(self):
         return Animal.objects.all().prefetch_related('medical_records', 'photos')
-
-    def perform_create(self, serializer):
-        serializer.save(created_by=self.request.user)
+    
+    @action(detail=True, methods=['get'])
+    def medical_records(self, request, pk=None):
+        animal = self.get_object()
+        records = animal.medical_records.all()
+        serializer = MedicalRecordSerializer(records, many=True)
+        return Response(serializer.data)
+    
+    @action(detail=True, methods=['get'])
+    def photos(self, request, pk=None):
+        animal = self.get_object()
+        photos = animal.photos.all()
+        serializer = AnimalPhotoSerializer(photos, many=True)
+        return Response(serializer.data)
 
 class MedicalRecordViewSet(viewsets.ModelViewSet):
     serializer_class = MedicalRecordSerializer
-    permission_classes = [IsAdminOrEmployeeReadOnly]
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
-    filterset_fields = ['record_type', 'animal']
-    ordering = ['-date_recorded']
-
+    permission_classes = [IsStaffOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['animal', 'record_type', 'date_recorded']
+    search_fields = ['description', 'animal__name', 'animal__chip_id']
+    
     def get_queryset(self):
-        return MedicalRecord.objects.all().select_related('animal', 'created_by')
-
-    def perform_create(self, serializer):
-        serializer.save(created_by=self.request.user)
+        return MedicalRecord.objects.all().select_related('animal')
 
 class AnimalPhotoViewSet(viewsets.ModelViewSet):
     serializer_class = AnimalPhotoSerializer
-    permission_classes = [IsAdminOrEmployeeReadOnly]
+    permission_classes = [IsStaffOrReadOnly]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['animal', 'is_primary']
-
+    
     def get_queryset(self):
-        return AnimalPhoto.objects.all().select_related('animal', 'uploaded_by')
-
-    def perform_create(self, serializer):
-        serializer.save(uploaded_by=self.request.user)
+        return AnimalPhoto.objects.all().select_related('animal')
--- animals/views.py
+++ animals/views.py
@@ -1,68 +1,61 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.http import Http404
 from .models import Animal, MedicalRecord, AnimalPhoto
 from .forms import AnimalForm, MedicalRecordForm, AnimalPhotoForm
-from shelter.models import ShelterUser
 
 class ShelterPermissionMixin(LoginRequiredMixin):
-    """Mixin to check shelter permissions"""
+    """Mixin to check shelter permissions - simplified for single-tenant"""
     
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             return self.handle_no_permission()
         
-        try:
-            self.shelter_user = ShelterUser.objects.get(user=request.user)
-        except ShelterUser.DoesNotExist:
-            raise Http404("User not associated with any shelter")
-        
+        # All authenticated users can access
         return super().dispatch(request, *args, **kwargs)
 
-class AdminOnlyMixin(ShelterPermissionMixin):
-    """Mixin for admin-only views"""
+class AdminOnlyMixin(UserPassesTestMixin):
+    """Mixin for admin-only views - uses Django's staff/superuser"""
     
-    def dispatch(self, request, *args, **kwargs):
-        response = super().dispatch(request, *args, **kwargs)
-        if self.shelter_user.role != 'admin':
-            raise Http404("Admin access required")
-        return response
+    def test_func(self):
+        return self.request.user.is_staff or self.request.user.is_superuser
 
 class AnimalListView(ShelterPermissionMixin, ListView):
     model = Animal
     template_name = 'animals/animal_list.html'
     context_object_name = 'animals'
     paginate_by = 20
-    
+
     def get_queryset(self):
+        from django.db import models
         queryset = Animal.objects.all().prefetch_related('photos', 'medical_records')
-        
+
         # Apply filters
         species = self.request.GET.get('species')
         if species:
             queryset = queryset.filter(species=species)
-            
+
         adoption_status = self.request.GET.get('adoption_status')
         if adoption_status:
             queryset = queryset.filter(adoption_status=adoption_status)
-            
+
         search = self.request.GET.get('search')
         if search:
             queryset = queryset.filter(
                 models.Q(name__icontains=search) |
                 models.Q(chip_id__icontains=search)
             )
-        
+
         return queryset.order_by('-created_at')
 
 class AnimalDetailView(ShelterPermissionMixin, DetailView):
     model = Animal
     template_name = 'animals/animal_detail.html'
     context_object_name = 'animal'
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['medical_records'] = self.object.medical_records.all()
@@ -74,7 +67,7 @@ class AnimalCreateView(ShelterPermissionMixin, CreateView):
     form_class = AnimalForm
     template_name = 'animals/animal_form.html'
     success_url = reverse_lazy('animals:animal_list')
-    
+
     def form_valid(self, form):
         form.instance.created_by = self.request.user
         messages.success(self.request, f'Animal {form.instance.name} created successfully!')
@@ -84,10 +77,10 @@ class AnimalUpdateView(ShelterPermissionMixin, UpdateView):
     model = Animal
     form_class = AnimalForm
     template_name = 'animals/animal_form.html'
-    
+
     def get_success_url(self):
         return reverse_lazy('animals:animal_detail', kwargs={'pk': self.object.pk})
-    
+
     def form_valid(self, form):
         messages.success(self.request, f'Animal {form.instance.name} updated successfully!')
         return super().form_valid(form)
@@ -96,7 +89,7 @@ class AnimalDeleteView(AdminOnlyMixin, DeleteView):
     model = Animal
     template_name = 'animals/animal_confirm_delete.html'
     success_url = reverse_lazy('animals:animal_list')
-    
+
     def delete(self, request, *args, **kwargs):
         messages.success(request, f'Animal {self.get_object().name} deleted successfully!')
         return super().delete(request, *args, **kwargs)
@@ -105,20 +98,20 @@ class MedicalRecordCreateView(ShelterPermissionMixin, CreateView):
     model = MedicalRecord
     form_class = MedicalRecordForm
     template_name = 'animals/medical_record_form.html'
-    
+
     def dispatch(self, request, *args, **kwargs):
         self.animal = get_object_or_404(Animal, pk=kwargs['animal_id'])
         return super().dispatch(request, *args, **kwargs)
-    
+
     def form_valid(self, form):
         form.instance.animal = self.animal
         form.instance.created_by = self.request.user
         messages.success(self.request, 'Medical record added successfully!')
         return super().form_valid(form)
-    
+
     def get_success_url(self):
         return reverse_lazy('animals:animal_detail', kwargs={'pk': self.animal.pk})
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['animal'] = self.animal
@@ -128,14 +121,14 @@ class AnimalPhotoListView(ShelterPermissionMixin, ListView):
     model = AnimalPhoto
     template_name = 'animals/animal_photos.html'
     context_object_name = 'photos'
-    
+
     def dispatch(self, request, *args, **kwargs):
         self.animal = get_object_or_404(Animal, pk=kwargs['animal_id'])
         return super().dispatch(request, *args, **kwargs)
-    
+
     def get_queryset(self):
         return AnimalPhoto.objects.filter(animal=self.animal)
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['animal'] = self.animal
@@ -148,7 +141,7 @@ class PublicAdoptionView(ListView):
     template_name = 'public/adoption_list.html'
     context_object_name = 'animals'
     paginate_by = 12
-    
+
     def get_queryset(self):
         return Animal.objects.filter(
             public_visibility=True,
@@ -159,7 +152,7 @@ class PublicAnimalDetailView(DetailView):
     model = Animal
     template_name = 'public/animal_detail.html'
     context_object_name = 'animal'
-    
+
     def get_queryset(self):
         return Animal.objects.filter(public_visibility=True)
 
